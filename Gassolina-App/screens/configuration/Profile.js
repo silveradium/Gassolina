563181ddf89fbcbb2778c110eedac477e4a70312
Agra Navaratne <agranavaratne@gmail.com>
2024-06-14 00:46:22 +0530

    added background

--- Gassolina-App/screens/configuration/Profile.js
+++ Gassolina-App/screens/configuration/Profile.js
@@ -1,15 +1,31 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet, Image } from 'react-native';
 
 
-export default function Profile({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Main')}
-      />
-    </View>
-  );
-}
+export default function Profile({ navigation}) {
+
+      return (
+        <View style={styles.container}> 
+            <Image source={require('../../assets/background.png')} style={ styles.background } />
+            <Text>Wifi</Text>
+            <Button
+                title="Go to Home"
+                onPress={() => navigation.navigate('Main')}
+            />
+        </View>
+      );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    background: {
+        position: 'absolute',
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+}})
