75ea6c79e7932b83ccbf73a7d85b7699183ebe21
Agra Navaratne <agranavaratne@gmail.com>
2024-06-16 04:38:27 +0530

    updated home

--- Gassolina-App/screens/main/Home.js
+++ Gassolina-App/screens/main/Home.js
@@ -1,11 +1,49 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 
-export default function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-    </View>
-  );
-}
+export default function Home() {
+
+      return (
+        <View style={styles.container}> 
+            <Image source={require('../../assets/background.png')} style={ styles.background } />
+            <View style={styles.top}>
+                <Text style={styles.name}>Hi Agra,</Text>
+                <Text style={styles.nameDescription}>Litro Gas Cylinder, 12.5kg</Text>
+            </View>
+            
+
+        </View>
+      );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //alignItems: 'center',
+        //justifyContent: 'center',
+    },
+    background: {
+        position: 'absolute',
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+    },
+    top: {
+        marginTop: 50,
+        marginLeft: 20,
+    },
+    name: {
+        color: '#4E4B4F',
+        fontFamily: "Poppins-Medium",
+        fontSize: 30,
+        lineHeight: 'normal',
+    },
+    nameDescription: {
+        color: 'rgba(0, 0, 0, 0.43)',
+        fontFamily: "MontserratAlternates-Semibold",
+        fontSize: 11,
+        letterSpacing: 0.44,
+    }
+})
