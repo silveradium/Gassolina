da9aafff9d5fac356ea93cfe3e5400a01b2b02b9
Agra Navaratne <agranavaratne@gmail.com>
2024-06-16 00:43:50 +0530

    firebase good

--- Gassolina-App/screens/configuration/Sign-up.js
+++ Gassolina-App/screens/configuration/Sign-up.js
@@ -1,5 +1,27 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+const onPress = () => {
+  auth()
+  .createUserWithEmailAndPassword('janiee.doe@example.com', 'SuperSecretPassword!')
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+}
+
+
 
 
 export default function SignUp({ navigation }) {
@@ -9,6 +31,10 @@ export default function SignUp({ navigation }) {
       <Button
         title="Go to Wifi"
         onPress={() => navigation.navigate('Wifi')}
+      />
+      <Button
+        title="Go to Wifi"
+        onPress={onPress}
       />
     </View>
   );
